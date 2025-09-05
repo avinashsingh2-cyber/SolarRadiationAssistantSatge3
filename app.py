8f2d9508b4953b4d0c53a1d79a7575809102f441
avinashsingh2-cyber <avinash.singh2@avaada.com>
2025-09-05 10:53:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def fuzzy_match_best(query, choices):
 
 def extract_top_n(query):
     try:
-        return int(re.findall(r"top (\d+)", query.lower())[0])
+        return int(re.findall(r"top (\\d+)", query.lower())[0])
     except:
         return 5
 
